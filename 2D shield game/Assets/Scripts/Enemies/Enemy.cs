d3293a3e758fd1895da911549849579920210e49
mo214885 <mo214885@falmouth.ac.uk>
2020-10-08 20:29:58 +0100

    Implementing enemy base class and Eyebat.cs which inherits from the base class.
    Signed-off-by: mo214885 <mo214885@falmouth.ac.uk>

---
+++ 2D shield game/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+    [HideInInspector]
+    public enum States {Idle, Attack};
+    [HideInInspector]
+    public States states;
+    [HideInInspector]
+    Rigidbody2D rb;
+    [HideInInspector]
+    bool completeAction;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void behvaiour(States state)
+    {
+        switch (state)
+        {
+            case States.Idle:
+                break;
+            case States.Attack:
+                break;
+            default:
+
+                break;
+        }
+    }
+
+    public virtual void Idle()
+    {
+
+    }
+
+    public virtual void Move()
+    {
+
+    }
+}
---
+++ 2D shield game/Assets/Scripts/Enemies/Eyebat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyebat : Enemy
+{
+    DealDamage dd;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+        dd = GetComponent<DealDamage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
