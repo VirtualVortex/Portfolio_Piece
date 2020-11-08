efdff1823175da395cfe6ea7619764aee9b71113
mo214885 <mo214885@falmouth.ac.uk>
2020-11-08 17:16:44 +0000

    pushing.
    Signed-off-by: mo214885 <mo214885@falmouth.ac.uk>

--- 2D shield game/Assets/Scripts/Managers/MasterClass.cs
+++ 2D shield game/Assets/Scripts/Managers/MasterClass.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MasterClass : MonoBehaviour
 {
@@ -20,10 +21,39 @@ public class MasterClass : MonoBehaviour
     [SerializeField]
     public Health health { get; private set; }
 
+    string sceneName;
+
     private void Awake()
     {
         inst = this;
+        Singleton();
 
+        SetUp();
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+
+    void Update()
+    {
+        if (sceneName != SceneManager.GetActiveScene().name)
+        {
+            SetUp();
+            sceneName = SceneManager.GetActiveScene().name;
+        }
+    }
+
+    void Singleton()
+    {
+        //Singleton
+        if(inst == null)
+            inst = this;
+        else if(inst != this)
+            Destroy(FindObjectOfType<SceneChanger>().gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void SetUp()
+    {
         player = GameObject.Find("Player");
         rb = player.GetComponent<Rigidbody2D>();
         sr = player.GetComponent<SpriteRenderer>();
@@ -31,6 +61,4 @@ public class MasterClass : MonoBehaviour
         climbing = player.GetComponent<Climbing>();
         health = player.GetComponent<Health>();
     }
-
-    
 }
---
+++ 2D shield game/Assets/Scripts/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Singleton : MonoBehaviour
+{
+    public static Singleton inst;
+
+    string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneName = SceneManager.GetActiveScene().name;
+
+        //Singleton
+        if (inst == null)
+            inst = this;
+        else if (inst != this)
+            Destroy(FindObjectOfType<Singleton>().gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sceneName != SceneManager.GetActiveScene().name)
+        {
+            SetUp();
+            sceneName = SceneManager.GetActiveScene().name;
+        }
+    }
+
+    IEnumerator SetUp()
+    {
+        foreach (Transform child in transform.GetComponentInChildren<Transform>())
+            child.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        foreach (Transform child in transform.GetComponentInChildren<Transform>())
+            child.gameObject.SetActive(true);
+    }
+}
